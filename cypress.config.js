933d3ee22ca4178d823e679ec6b0f483fe500a9c
Emilio Ramírez Mascarúa <ramarm0825@gmail.com>
2025-04-23 11:56:31 -0600

    Revert "Feature/tests"

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:5173", // Vite's default port
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-    supportFile: "cypress/support/e2e.ts",
-  },
-});
